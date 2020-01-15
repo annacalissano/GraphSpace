dfb75e2a2be51fd3236ed6b2bcef3c59e6df2873
annacalissano <annacalissano1@gmail.com>
2020-01-15 15:35:32 +0100

    Update __init__.py

--- AlignCompute/__init__.py
+++ AlignCompute/__init__.py
@@ -1,6 +1,3 @@
 from .aligncompute import aligncompute
 from .meanac import meanac
-from .pgaac import pgaac
-from .pgaac_vector import pgaac_vector
-from .regressionac_vector import regressionac_vector
-from .gnmf_vector import gnmf_vector
+from .gpc_aac import gpc_aac
